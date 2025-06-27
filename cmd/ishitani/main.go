214396d7d2a8cdc0aad75c208a04a2aa9a10d41d
willywotz <dogajack@gmail.com>
2025-06-28 06:22:36 +0700

    Add Cosign integration for binary signing and verification; implement machineID function for unique identification

--- cmd/ishitani/main.go
+++ cmd/ishitani/main.go
@@ -1,17 +1,45 @@
 package main
 
 import (
-	"time"
+	"fmt"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
-	if !checkAdmin() {
-		_ = becomeAdmin()
-		return
-	}
+	// if !checkAdmin() {
+	// 	_ = becomeAdmin()
+	// 	return
+	// }
 
-	time.Sleep(2 * time.Second) // Simulate some startup delay
-	PressAndRelease(VK_E)
+	// time.Sleep(2 * time.Second) // Simulate some startup delay
+	// PressAndRelease(VK_E)
+
+	myApp := app.New()
+	myWindow := myApp.NewWindow("List Data")
+
+	data := binding.BindStringList(
+		&[]string{"Item 1", "Item 2", "Item 3"},
+	)
+
+	list := widget.NewListWithData(data,
+		func() fyne.CanvasObject {
+			return widget.NewLabel("template")
+		},
+		func(i binding.DataItem, o fyne.CanvasObject) {
+			o.(*widget.Label).Bind(i.(binding.String))
+		})
+
+	add := widget.NewButton("Append", func() {
+		val := fmt.Sprintf("Item %d", data.Length()+1)
+		data.Append(val)
+	})
+	myWindow.SetContent(container.NewBorder(nil, add, nil, nil, list))
+	myWindow.ShowAndRun()
 }
 
 // func main() {
