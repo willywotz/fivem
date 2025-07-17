ea5da1fb4c14d918f130e288a6ee4e797ea58848
willywotz <dogajack@gmail.com>
2025-07-17 12:54:40 +0700

    Add 'From' field to Status struct for enhanced status tracking

--- server/main.go
+++ server/main.go
@@ -14,6 +14,7 @@ type Status struct {
 	Hostname  string    `json:"hostname"`
 	Username  string    `json:"username"`
 	IP        string    `json:"ip"`
+	From      string    `json:"from"`
 	Time      time.Time `json:"time"`
 }
 
